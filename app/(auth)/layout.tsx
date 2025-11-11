1cb7d88c733111e071d7bcdf6f8c97aeed45f30e
João Vitor <joaovvsantos77@gmail.com>
2025-11-10 23:50:11 -0300

    Implementação do Auth UI e do Layout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Layout: () => Element = ({children}: {children: React.ReactNode }) => {
+    return (
+        <main className="auth-layout">
+            <section className="auth-left-section scrollbar-hide-default">
+                <Link href="/" className="auth-logo">
+                    <Image src="/assets/icons/logo.svg" alt="Signalist Logo" width={140} height={32} className="h-8 w-auto" />
+                </Link>
+
+                <div className="pb-6 lg:pb-8 flex-1">{children}</div>
+            </section>
+
+            <section className="auth-right-section">
+                <div className="z-10 relative lg:mt-4 lg:mb-16">
+                    <blockquote className="auth-blockquote">
+                        A Signalist transformou minha lista de observação em uma lista de ganhos. Os alertas são precisos e eu me sinto mais confiante ao tomar decisões no mercado.
+                    </blockquote>
+                    <div className="flex items-center justify-between">
+                        <div>
+                            <cite className="auth-testimonial-author">- João V.</cite>
+                            <p className="max-md:text-xs text-gray-500">Investidor Anjo</p>
+                        </div>
+                        <div className="flex items-center gap-0.5">
+                            {[1, 2, 3, 4, 5].map((star) => (
+                                <Image src="/assets/icons/star.svg" alt="Star" key={star} width={20} height={20} className="w-5 h-5" />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex-1 relative">
+                    <Image src="/assets/images/dashboard.png" alt="Dashboard Preview" width={1440} height={1150} className="auth-dashboard-preview absolute top-0" />
+                </div>
+            </section>
+        </main>
+    )
+}
+
+export default Layout
